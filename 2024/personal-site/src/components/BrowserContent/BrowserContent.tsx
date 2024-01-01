3738d6cbc68c5847f2e1d46cd214f2fb564ed6e1
kick-push-coast <mktyler01@gmail.com>
2023-12-31 17:43:44 -0800

    Updating scroll behavior

--- 2024/personal-site/src/components/BrowserContent/BrowserContent.tsx
+++ 2024/personal-site/src/components/BrowserContent/BrowserContent.tsx
@@ -1,11 +1,24 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import { BrowserArticle, ArticleTopic } from '../BrowserArticle';
 import classes from './browser-content.module.scss';
+import { useEffect, useRef } from 'react';
 
 export const BrowserContent = () => {
+
+    const container = useRef<HTMLElement>(null);
+    const location = useLocation();
+    const scrollOnNavigate = () => {
+        if (container.current) {
+            container.current.scrollTo(0,0);
+        }
+    }
+
+    useEffect(() => {
+        scrollOnNavigate()
+    }, [location]);
 	
 	return (
-		<main className={classes.container}>
+		<main ref={container} className={classes.container}>
             <Routes>
                 <Route path="/" element={<BrowserArticle topic={ArticleTopic.Home} />} />
                 <Route path="/skills" element={<BrowserArticle topic={ArticleTopic.Skills} />} />
