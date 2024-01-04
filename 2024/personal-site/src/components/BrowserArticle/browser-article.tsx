c0536281b7a50fbc36a4e0c02832a75e0e198a82
kick-push-coast <mktyler01@gmail.com>
2024-01-03 22:37:51 -0800

    Updating copy

--- 2024/personal-site/src/components/BrowserArticle/browser-article.tsx
+++ 2024/personal-site/src/components/BrowserArticle/browser-article.tsx
@@ -18,7 +18,7 @@ const topics = new Map<ArticleTopic, ReactNode>([
             My name is Mike
         </h3>
         <p>
-            I have 6+ years of experience as a <strong>Software Engineer</strong> with a frontend focus + 1 year of experience as a <strong>Technical PM</strong>, all in a startup/scale-up environment
+            I have 5+ years of experience as a <strong>Software Engineer</strong> with a frontend focus + 1 year of experience as a <strong>Technical PM</strong>, all in a startup/scale-up environment. I enjoy writing practical, robust code that helps users accomplish tasks and fluidly interact with systems.
         </p>
         <p className={classes.hideMobile}>
             Tab around a bit to get to know me :-)
@@ -62,6 +62,7 @@ const topics = new Map<ArticleTopic, ReactNode>([
             <li>Vite</li>
             <li>Jest</li>
             <li>Jasmine + Karma</li>
+            <li>Swagger</li>
             <li>Figma</li>
             <li>Storybook</li>
             <li>Jira</li>
@@ -73,6 +74,7 @@ const topics = new Map<ArticleTopic, ReactNode>([
             <li>Accessibility</li>
             <li>Responsive Design</li>
             <li>Software Design Patterns</li>
+            <li>REST Principles</li>
             <li>UX/UI Fundamentals</li>
             <li>Competitor Analysis</li>
             <li>User Research</li>
@@ -91,44 +93,44 @@ const topics = new Map<ArticleTopic, ReactNode>([
             Sep 2017 - Present
         </p>
         <p>
-            Cludo offers a collection of SaaS products for building and managing site search. These include automated data source scraping, developer toolsets for creating custom search solutions, and an admin web app for configuring search indices and search relevance.
+        Cludo provides B2B SaaS products for enterprise-level site search, offering automated data source scraping, developer toolsets, and an admin web app for configuring search indices and relevance.
         </p>
         <ul>
             <li>
                 <strong>Technical Product Manager</strong><span className={classes.hideMobile}> | </span><span className={classes.hideDesktop}><br/></span>Dec 2022 - Present
                 <ul>
-                    <li>Led a team of engineers focused on building developer tools</li>
-                    <li>Collaborated with partner agencies to design practical component sets and APIs that meet real developer needs</li>
-                    <li>Built and released a React SDK that decreased development time for search UI implementations by up to 30% (documented at: <a href="//react.cludo.com" target="_blank">react.cludo.com</a>)</li>
-                    <li>Wrote public-facing technical documentation for various toolsets (cataloged at: <a href="//www.cludo.com/cludo-developer-experience-0?hs_preview=PCthItJq-87790382543" target="_blank">cludo.com/cludo-developer-experience</a>)</li>
-                    <li>Continued to contribute code, code review, and consultation on software design patterns</li>
+                    <li>Led a team of engineers focused on creating developer tools</li>
+                    <li>Collaborated with partner agencies to design practical component sets and APIs</li>
+                    <li>Developed and launched a React SDK reducing search UI implementation time by up to 30% (documented at: <a href="//react.cludo.com" target="_blank">react.cludo.com</a>)</li>
+                    <li>Authored public-facing technical documentation for developer toolsets (cataloged at: <a href="//www.cludo.com/cludo-developer-experience-0?hs_preview=PCthItJq-87790382543" target="_blank">cludo.com/cludo-developer-experience</a>)</li>
+                    <li>Continued code contributions, code reviews, and software design consultations</li>
                 </ul>
             </li>
             <br/>
             <li>
                 <strong>Lead UX Engineer</strong><span className={classes.hideMobile}> | </span><span className={classes.hideDesktop}><br/></span>May 2021 - Dec 2022
                 <ul>
-                    <li>Worked jointly with the design team on a near-complete overhaul of the Cludo admin web app</li>
-                    <li>Iteratively refined the most essential user journeys leading to an improvement in measured user sentiment</li>
-                    <li>Maintained parity between components in Figma and components in code</li>
+                    <li>Collaborated on revamping the Cludo admin web app with the design team</li>
+                    <li>Improved critical user journeys, positively impacting measured user sentiment</li>
+                    <li>Maintained parity between Figma-designed components and code</li>
                 </ul>
             </li>
             <br/>
             <li>
                 <strong>Product Software Engineer</strong><span className={classes.hideMobile}> | </span><span className={classes.hideDesktop}><br/></span>Jun 2019 - May 2021
                 <ul>
-                    <li>Contributed as a core engineer for the Cludo admin web app built with Angular and TypeScript</li>
-                    <li>Applied various software design patterns to keep codebase robust and maintainable</li>
-                    <li>Ensured broad test coverage with Jasmine and Karma</li>
+                    <li>Contributed as a core engineer for the Cludo admin web app using Angular and TypeScript</li>
+                    <li>Applied software design patterns for code robustness and maintainability</li>
+                    <li>Ensured comprehensive test coverage with Jasmine and Karma</li>
                 </ul>
             </li>
             <br/>
             <li>
                 <strong>Frontend Developer</strong><span className={classes.hideMobile}> | </span><span className={classes.hideDesktop}><br/></span>Sep 2017 - Jun 2019
                 <ul>
-                    <li>Worked with clients to create custom search implementations according to their specifications</li>
-                    <li>Built re-usable search UI templates in the ASP.NET web framework</li>
-                    <li>Provided ongoing technical support and consultation for clients</li>
+                    <li>Worked jointly with clients to create tailored search implementations</li>
+                    <li>Developed reusable search UI templates in ASP.NET</li>
+                    <li>Offered ongoing technical support and consultation for clients</li>
                 </ul>
             </li>
         </ul>
@@ -141,7 +143,7 @@ const topics = new Map<ArticleTopic, ReactNode>([
             Dec 2015 - Aug 2017
         </p>
         <p>
-            Offered freelance web development services primarily focused on CMS-backed content websites for small company clients.
+            Provided freelance web development services primarily focused on CMS-backed content websites for small company clients.
         </p>
         <br/>
         <br/>
@@ -170,11 +172,11 @@ const topics = new Map<ArticleTopic, ReactNode>([
         </p>
     </>]
 ])
-export const BrowserArticle = (props: { topic: ArticleTopic }) => {
 
+export const BrowserArticle = (props: { topic: ArticleTopic }) => {
     return (
         <article className={classes.container}>
-            {topics.get(props.topic)}
+            { topics.get(props.topic) }
         </article>
     );
 };
