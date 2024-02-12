57ab9188e822e077a57f7ed5857e19e9d9752d92
kick-push-coast <mktyler01@gmail.com>
2024-02-11 20:38:48 -0800

    Updating server file

--- drawing-board/drawing-board-server/server.js
+++ drawing-board/drawing-board-server/server.js
@@ -4,7 +4,6 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
-    path: '/drawing-socket',
     cors: {
         origin: '*',
     }
