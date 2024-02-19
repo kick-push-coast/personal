6c18fb9c10b4fe5e4ffc2b693fa41f896a173988
kick-push-coast <mktyler01@gmail.com>
2024-02-18 19:52:13 -0800

    Fixing color picker open/close behavior

--- drawing-board/drawing-board-client/src/components/BoardToolbar/BoardToolbar.tsx
+++ drawing-board/drawing-board-client/src/components/BoardToolbar/BoardToolbar.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { MouseEvent, useContext, useEffect, useRef, useState } from "react";
 import { LineContextInstance } from "../BoardContainer/BoardContainer";
 import { LineDashPicker } from "../LineDashPicker";
 import useClickOutside from "../../hooks/use-click-outside";
@@ -17,6 +17,18 @@ export const BoardToolbar = () => {
     const lineContext = useContext(LineContextInstance);
     const registerClickOutside = useClickOutside();
 
+    function handleColorClick(e: MouseEvent) {
+        if (e.target instanceof HTMLElement && e.target.nodeName === 'INPUT') {
+            return;
+        }
+        if (colorOpen === false) {
+            setColorOpen(true);
+        } else {
+            e.preventDefault();
+            setColorOpen(false);
+        }
+    }
+
     useEffect(() => {
         colorRef.current && registerClickOutside(colorRef.current, () => setColorOpen(false));
         widthRef.current && registerClickOutside(widthRef.current, () => setWidthOpen(false));
@@ -26,14 +38,14 @@ export const BoardToolbar = () => {
     return (
         <div className={classes.container}>
             <div ref={colorRef}>
-                <label tabIndex={0} onClick={() => setColorOpen(true)} className={classes.label + (colorOpen ? ' ' + classes.labelOpen : '')}>
+                <label tabIndex={0} onClick={handleColorClick} className={classes.label + (colorOpen ? ' ' + classes.labelOpen : '')}>
                     Color
                     <input className={classes.inputColor} type="color" defaultValue={lineContext.color} onChange={(e) => lineContext.updateColor(e.target.value)} />
                 </label>
             </div>
 
             <div ref={widthRef} className={classes.option}>
-                <label tabIndex={0} onClick={() => setWidthOpen(true)} className={classes.label + (widthOpen ? ' ' + classes.labelOpen : '')}>
+                <label tabIndex={0} onClick={() => setWidthOpen(!widthOpen)} className={classes.label + (widthOpen ? ' ' + classes.labelOpen : '')}>
                     Width
                 </label>
                 <div className={classes.inputContainer + ' ' + classes.inputContainerRotated + (widthOpen ? ' ' + classes.inputOpen : '')}>
@@ -42,7 +54,7 @@ export const BoardToolbar = () => {
             </div>
 
             <div ref={dashRef} className={classes.option}>
-                <label tabIndex={0} onClick={() => setDashOpen(true)} className={classes.label + (dashOpen ? ' ' + classes.labelOpen : '')}>
+                <label tabIndex={0} onClick={() => setDashOpen(!dashOpen)} className={classes.label + (dashOpen ? ' ' + classes.labelOpen : '')}>
                     Dash
                 </label>
                 <div className={classes.inputContainer + (dashOpen ? ' ' + classes.inputOpen : '')}>
