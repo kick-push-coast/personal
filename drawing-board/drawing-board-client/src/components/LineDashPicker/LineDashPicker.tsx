2bd90010479272ad2928eca953a93b9a8b4719cb
kick-push-coast <mktyler01@gmail.com>
2024-02-18 21:59:44 -0800

    Finishing up tooling iconography

--- drawing-board/drawing-board-client/src/components/LineDashPicker/LineDashPicker.tsx
+++ drawing-board/drawing-board-client/src/components/LineDashPicker/LineDashPicker.tsx
@@ -1,6 +1,9 @@
 import { useContext } from 'react';
 import { LineDash } from '../../context/line-context';
 import { LineContextInstance } from '../BoardContainer/BoardContainer';
+import lineNoneSvg from "../../assets/line-None.svg";
+import lineShortSvg from "../../assets/line-Short.svg";
+import lineLongSvg from "../../assets/line-Long.svg";
 import classes from './line-dash-picker.module.scss';
 
 export interface LineDashPickerProps { }
@@ -10,37 +13,39 @@ export const LineDashPicker = () => {
 
     return (
         <>
-
-            <label className={classes.labelRadio} htmlFor="dash-input-none">
-                None
-            </label>
             <input
                 onChange={(e) => lineContext.updateDash(e.target.value as LineDash)}
                 defaultChecked={lineContext.dashType === LineDash.none}
+                className={classes.input}
                 id="dash-input-none"
                 name="dash-inputs"
                 type="radio"
                 value={LineDash.none} />
-            <label className={classes.labelRadio} htmlFor="dash-input-short">
-                Short
+            <label className={classes.labelRadio} htmlFor="dash-input-none">
+                <img src={lineNoneSvg}/>
             </label>
             <input
                 onChange={(e) => lineContext.updateDash(e.target.value as LineDash)}
                 defaultChecked={lineContext.dashType === LineDash.short}
+                className={classes.input}
                 id="dash-input-short"
                 name="dash-inputs"
                 type="radio"
                 value={LineDash.short} />
-            <label className={classes.labelRadio} htmlFor="dash-input-long">
-                Long
+            <label className={classes.labelRadio} htmlFor="dash-input-short">
+                <img src={lineShortSvg}/>
             </label>
             <input
                 onChange={(e) => lineContext.updateDash(e.target.value as LineDash)}
                 defaultChecked={lineContext.dashType === LineDash.long}
+                className={classes.input}
                 id="dash-input-long"
                 name="dash-inputs"
                 type="radio"
                 value={LineDash.long} />
+            <label className={classes.labelRadio} htmlFor="dash-input-long">
+                <img src={lineLongSvg}/>
+            </label>
         </>
     );
 };
--- drawing-board/drawing-board-client/src/context/line-context.ts
+++ drawing-board/drawing-board-client/src/context/line-context.ts
@@ -12,7 +12,7 @@ export class LineContext {
 
     constructor () {
         // default values
-        this.width = 10;
+        this.width = 12;
         this.dashType = LineDash.none;
         this.dashValue = [0, 0];
         this.color = '#000000';
