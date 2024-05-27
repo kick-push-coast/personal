0e71bae02b5d37c7e21c98c9d1cf588b02d88e35
kick-push-coast <mktyler01@gmail.com>
2024-05-26 17:37:27 -0700

    Adding generate prompt component

---
+++ drawing-board/drawing-board-client/src/components/BoardToolbar/options/GeneratePrompt/GeneratePrompt.tsx
@@ -0,0 +1,73 @@
+import { useRef, useState, useEffect, FormEvent } from 'react';
+import useClickOutside from '../../../../hooks/use-click-outside';
+import openAiSvg from '../../../../assets/openai.svg';
+import classes from '../../board-toolbar.module.scss';
+import formClasses from './generate-prompt.module.scss';
+import { generateImage } from '../../../../helpers/generate-helpers';
+
+interface GeneratePromptProps {
+    onImageGenerate: (image: ImageData | undefined) => void
+}
+
+let generateTimer: NodeJS.Timeout;
+
+export const GeneratePrompt = (props: GeneratePromptProps) => {
+    
+    const registerClickOutside = useClickOutside();
+    const generateRef = useRef<HTMLDivElement>(null);
+    const [generateOpen, setGenerateOpen] = useState(false);
+    const [generateLoading, setGenerateLoading] = useState(false);
+    const [generateLabel, setGenerateLabel] = useState(<>Generate</>);
+
+    useEffect(() => {
+        generateRef.current && registerClickOutside(generateRef.current, () => setGenerateOpen(false));
+    }, [])
+
+    async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        const form = e.target as HTMLFormElement;
+        const input = form.elements[0] as HTMLInputElement;
+        setGenerateLoading(true);
+        const image = await generateImage(input.value);
+        setGenerateLoading(false);
+        props.onImageGenerate(image);
+        setGenerateOpen(false);
+    }
+
+    useEffect(() => {
+        if (generateLoading) {
+            let i = 1;
+            const labels = [
+                <>&nbsp;&nbsp;&nbsp;Generating&nbsp;&nbsp;&nbsp;</>,
+                <>&nbsp;&nbsp;&nbsp;Generating.&nbsp;&nbsp;</>,
+                <>&nbsp;&nbsp;&nbsp;Generating..&nbsp;</>,
+                <>&nbsp;&nbsp;&nbsp;Generating...</>,
+            ]
+            setGenerateLabel(labels[0]);
+            generateTimer = setInterval(() => {
+                setGenerateLabel(labels[i]);
+                i = i < 3 ? i + 1 : 0;
+            }, 500);
+        } else {
+            clearInterval(generateTimer);
+            setGenerateLabel(<>Generate</>);
+        }
+    }, [generateLoading])
+    
+    return (
+        <div ref={generateRef} className={classes.option}>
+            <label tabIndex={0} onClick={() => setGenerateOpen(!generateOpen)} className={classes.label + (generateOpen ? ' ' + classes.labelOpen : '')}>
+                <img className={classes.dashImg} src={openAiSvg} />
+            </label>
+            <div className={classes.inputContainer + ' ' + (generateOpen ? ' ' + classes.inputOpen : '')}>
+                <form onSubmit={handleSubmit} className={classes.inputMargin}>
+                    <h2 className={formClasses.title}>&nbsp;&nbsp; Generate a drawing 🪄</h2>
+                    <input className={formClasses.input} name="prompt" type='text' placeholder='Enter a descriptive prompt' />
+                    <button className={formClasses.submit} type='submit'>
+                        {generateLabel}
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+};
