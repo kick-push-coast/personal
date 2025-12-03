72baf075f94236a81505b57fe44a3d58dd8c69c6
Michael Tyler <mktyler01@gmail.com>
2025-12-03 10:30:24 -0600

    Adding GA tag to built index.html

--- personal-site/vite.config.ts
+++ personal-site/vite.config.ts
@@ -3,7 +3,26 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'add-ga-tag',
+      transformIndexHtml(html) {
+        const gaTag = `
+          <!-- Google tag (gtag.js) -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-54KPCHN0LG"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-54KPCHN0LG');
+          </script>
+        `;
+        return html.replace('<head>', `<head>${gaTag}\n`);
+      },
+    },
+  ],
   test: {
     environment: 'jsdom',
     globals: true,
